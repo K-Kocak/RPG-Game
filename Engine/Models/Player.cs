11327a6c9c4c82f72d018ab5f35070d52fdb1ef8
K-Kocak <NA>
2023-06-17 15:46:54 +0100

    Update

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -1,37 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 namespace Engine.Models
 {
-    public class Player : BaseNotificationClass
+    public class Player : LivingEntity
     {
-
-        private string _name;
+        #region Properties
         private string _characterClass;
-        private int _hitPoints;
         private int _experiencePoints;
         private int _level;
-        private int _gold;
-
-
-
-        // Constructors
-        public string Name 
-        { 
-            get { return _name; }
-            set 
-            {
-                _name = value;
-                OnPropertyChanged(nameof(Name));
-            }
-        }
-
-
-        public string CharacterClass 
+        public string CharacterClass
         {
             get { return _characterClass; }
             set
@@ -40,67 +18,40 @@ namespace Engine.Models
                 OnPropertyChanged(nameof(CharacterClass));
             }
         }
-
-
-        public int HitPoints 
+        public int ExperiencePoints
         {
-            get { return _hitPoints; } 
-            set 
+            get { return _experiencePoints; }
+            set
             {
-                _hitPoints = value;
-                OnPropertyChanged(nameof(HitPoints));
-            }
-        }
-
-
-        public int ExperiencePoints 
-        {
-            get { return _experiencePoints; } 
-            set 
-            { 
                 _experiencePoints = value;
                 OnPropertyChanged(nameof(ExperiencePoints));
             }
         }
-
-
-
-        public int Level 
+        public int Level
         {
             get { return _level; }
             set
             {
                 _level = value;
                 OnPropertyChanged(nameof(Level));
-            } 
+            }
         }
-
-
-
-
-        public int Gold 
-        {
-            get { return _gold; }
-            set
-            {
-                _gold = value;
-                OnPropertyChanged(nameof(Gold));
-            } 
-        }
-
-      
-
-        public ObservableCollection<GameItem> Inventory { get; set; }
-
-
+        public ObservableCollection<QuestStatus> Quests { get; set; }
+        #endregion
         public Player()
         {
-            Inventory = new ObservableCollection<GameItem>();
+            Quests = new ObservableCollection<QuestStatus>();
         }
-
-
-
+        public bool HasAllTheseItems(List<ItemQuantity> items)
+        {
+            foreach (ItemQuantity item in items)
+            {
+                if (Inventory.Count(i => i.ItemTypeID == item.ItemID) < item.Quantity)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
-
-
 }
